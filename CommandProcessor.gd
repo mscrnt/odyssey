4c4b05fb5a281a26b28b13578dcb4057fb6bef27
Kenneth Blossom's Portfolio <33361957+mscrnt@users.noreply.github.com>
2023-12-21 19:05:04 -0800

    refactored exits

--- CommandProcessor.gd
+++ CommandProcessor.gd
@@ -42,7 +42,7 @@ func go(second_word: String) -> String:
 	if current_room.exits.keys().has(second_word):
 		var exit = current_room.exits[second_word]
 		if exit.is_other_room_locked(current_room):
-			return "That exit is currently locked!"
+			return "The way to the %s is currently locked!" % second_word
 			
 		var change_response = change_room(exit.get_other_room(current_room))
 		return PoolStringArray(["You go %s" % second_word + "\n", change_response]).join("\n")
@@ -85,7 +85,7 @@ func use(second_word: String) -> String:
 			match item.item_type:
 				Types.ItemTypes.KEY:
 					for exit in current_room.exits.values():
-						if exit.room_2 == item.use_value:
+						if exit == item.use_value:
 							exit.room_2_is_locked = false
 							player.drop_item(item)
 							return "You used %s to unlock a door to %s" % [item.item_name, exit.room_2.room_name]
--- rooms/RoomManager.gd
+++ rooms/RoomManager.gd
@@ -4,16 +4,32 @@ extends Node
 func _ready() -> void:
 	var key = Item.new()
 	key.initialize("key", Types.ItemTypes.KEY, "An old brass key.")
-	key.use_value = $"Secret Chamber"
+
+	
+	## World - Odyssey VR
+	
+	# Home
+	$"Home".connect_exit_unlocked("avalonia", $"Central Avalonia", "home")
 	
 	## World - AVALONIA
 	
 	# Central Avalonia
-	$"Central Avalonia".connect_exit_unlocked("north", $"Grand Castle")
+	$"Central Avalonia".connect_exit_unlocked("castle", $"Grand Castle", "avalonia")
+	$"Central Avalonia".connect_exit_unlocked("market", $"Mystic Market", "avalonia")
+	$"Central Avalonia".connect_exit_locked("sewers", $"Secret Chamber", "avalonia")
+	$"Central Avalonia".connect_exit_unlocked("elders", $"Hall of Elders", "avalonia")
+	
+	
+	# Mystic Market
+	# TODO
 	
 	# The Grand Castle
-	$"Grand Castle".connect_exit_unlocked("east", $"Royal Library")
+	$"Grand Castle".connect_exit_unlocked("throne", $"Throne Room", "castle")
+	$"Grand Castle".connect_exit_unlocked("library", $"Royal Library", "castle")
+	$"Grand Castle".connect_exit_unlocked("courtyard", $"Grand Courtyard", "castle")
 	
 	# The Royal Library
-	$"Royal Library".connect_exit_locked("east", $"Secret Chamber")
+	var exit = $"Royal Library".connect_exit_locked("chamber", $"Secret Chamber", "library")
+	key.use_value = exit
 	$"Royal Library".add_item(key)
+	
